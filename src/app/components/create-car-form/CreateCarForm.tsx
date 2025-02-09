9fdd4fcbcc707fc30c9cad593a56bac79d1276e3
fedorchaklm <fedorchak.luba@gmail.com>
2025-02-09 12:28:03 +0200

    hw3 with useActionsState

---
+++ src/app/components/create-car-form/CreateCarForm.tsx
@@ -0,0 +1,38 @@
+'use client';
+
+import {createCar, State} from "@/server-actions/serverActions";
+import {useActionState} from "react";
+
+const initialState = {
+    message: '',
+    errors: {}
+};
+
+const CreateCarForm = () => {
+    const [state, formAction] = useActionState<State, FormData>(createCar, initialState);
+    const brand = state.payload?.get("brand")?.toString() ?? '';
+    const price = state.payload?.get("price")?.toString() ?? 0;
+    const year = state.payload?.get("year")?.toString() ?? 0;
+
+    return (
+        <form action={formAction} method="POST"
+              className='flex flex-col items-center gap-4 text-2xl rounded-3xl w-fit px-8 py-4 m-auto my-4 bg-white'>
+            <h1 className='text-3xl'>Create car</h1>
+            <label htmlFor='brand'>Enter brand</label>
+            <input className='border-2 border-gray-500 rounded-xl block pl-4 py-2' type='text' id='brand' name='brand'
+                   placeholder='brand' defaultValue={brand}/>
+            {state.errors?.brand && <p className='text-red-500' aria-live="polite">{state.message}</p>}
+            <label htmlFor='price'>Enter price</label>
+            <input className='border-2 border-gray-500 rounded-xl block pl-4 py-2' type='number' id='price' name='price'
+                   placeholder='price' defaultValue={price}/>
+            {state.errors?.price && <p className='text-red-500' aria-live="polite">{state.message}</p>}
+            <label htmlFor='year'>Enter year</label>
+            <input className='border-2 border-gray-500 rounded-xl block pl-4 py-2' type='number' id='year' name='year'
+                   placeholder='year' defaultValue={year}/>
+            {state.errors?.year && <p className='text-red-500' aria-live="polite">{state.message}</p>}
+            <button className='bg-black text-white text-2xl px-2 py-2 rounded-xl' type="submit">ADD</button>
+        </form>
+    );
+};
+
+export default CreateCarForm;
--- src/server-actions/serverActions.ts
+++ src/server-actions/serverActions.ts
@@ -1,24 +1,51 @@
 'use server';
-
 import {carService} from "@/services/car.api.service";
 import {revalidatePath} from "next/cache";
 import {redirect} from "next/navigation";
+import {z} from 'zod';
 
-export const createCar = async (formData: FormData) => {
-        console.log(formData);
-        const brand = formData.get('brand') as string;
-        const year = Number(formData.get('year'));
-        const price = Number(formData.get('price'));
+export type State = {
+    payload?: FormData;
+    errors?: Partial<{
+        brand: string[];
+        year: string[];
+        price: string[];
+    }>;
+    message: string | null;
+};
 
-        const car = {brand, year, price};
-        console.log(car);
+const CarSchema = z.object({
+    brand: z.string()
+        .min(1, {message: 'Min length 1 character'}),
+    year: z.number(),
+    price: z.number(),
+});
 
-        try {
-            await carService.createCar(car);
-        } catch (e) {
-            console.log(e);
-        }
-        revalidatePath('/cars')
-        redirect('/cars');
+export const createCar = async (prevState: State, formData: FormData):  Promise<State> => {
+    console.log(formData);
+    // const brand = formData.get('brand') as string;
+    // const year = Number(formData.get('year'));
+    // const price = Number(formData.get('price'));
+
+    const validatedFields = CarSchema.safeParse({
+        brand: formData.get('brand'),
+        year: formData.get('year'),
+        price: formData.get('price'),
+    });
+
+    if (!validatedFields.success) {
+        return {
+            errors: validatedFields.error.flatten().fieldErrors,
+            message: 'Missing Fields',
+        };
     }
-;
+    // const car = CarSchema.parse({brand, year, price});
+    // console.log(car);
+    // const { brand, year, price } = validatedFields.data;
+    console.log(validatedFields.data);
+    await carService.createCar(validatedFields.data);
+
+    revalidatePath('/cars')
+    redirect('/cars');
+
+};
