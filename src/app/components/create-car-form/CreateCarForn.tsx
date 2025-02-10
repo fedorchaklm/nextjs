3e4327a609dd01144a0c6c5cd1a26c863bb3c63e
fedorchaklm <fedorchak.luba@gmail.com>
2025-02-10 17:22:16 +0200

    hw3

--- src/app/components/create-car-form/CreateCarForn.tsx
+++ src/app/components/create-car-form/CreateCarForn.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import {createCar} from "@/server-actions/serverActions";
+import {createCar, StateType} from "@/server-actions/serverActions";
 import {useForm} from "react-hook-form";
 import {ICar} from "@/models/ICar";
 // import {useRef} from "react";
@@ -12,8 +12,9 @@ import {useActionState} from "react";
 type CreateCar = Omit<ICar, "id">;
 
 const initialState = {
-    message: {}
-}
+    message: '',
+    errors: {}
+};
 
 const CreateCarForm = () => {
     const {handleSubmit, register, formState: {errors, isValid}} = useForm<CreateCar>({
@@ -22,15 +23,15 @@ const CreateCarForm = () => {
 
     console.log('> ', errors);
 
-    // const onSubmit = async (data: CreateCar) => {
-    //     console.log('> onSubmit', data);
-    //     const formData = new FormData();
-    //     formData.append('brand', data.brand);
-    //     formData.append('price', `${data.price}`);
-    //     formData.append('year', `${data.year}`);
-    // };
+    const onSubmit = async (data: CreateCar) => {
+        console.log('> onSubmit', data);
+        const formData = new FormData();
+        formData.append('brand', data.brand);
+        formData.append('price', `${data.price}`);
+        formData.append('year', `${data.year}`);
+    };
 
-    const [state, formAction] = useActionState(createCar, initialState)
+    const [state, formAction] = useActionState<StateType, FormData>(createCar, initialState)
 
     return (
         <Form action={formAction} onSubmit={handleSubmit(onSubmit)}
@@ -40,16 +41,19 @@ const CreateCarForm = () => {
             <input className='border-2 border-gray-500 rounded-xl block pl-4 py-2' type='text'
                    id='brand' {...register('brand')}
                    placeholder='brand'/>
+            <p className='text-red-500' aria-live="polite">{state.errors?.brand}</p>
             <div className='text-red-500'>{errors.brand?.message}</div>
             <label htmlFor='price'>Enter price</label>
             <input className='border-2 border-gray-500 rounded-xl block pl-4 py-2' type='number'
                    id='price' {...register('price')}
                    placeholder='price'/>
+            <p className='text-red-500' aria-live="polite">{state.errors?.price}</p>
             <div className='text-red-500'>{errors.price?.message}</div>
             <label htmlFor='year'>Enter year</label>
             <input className='border-2 border-gray-500 rounded-xl block pl-4 py-2' type='number'
                    id='year' {...register('year')}
                    placeholder='year'/>
+            <p className='text-red-500' aria-live="polite">{state.errors?.year}</p>
             <div className='text-red-500'>{errors.year?.message}</div>
             <button
                 className={isValid ? 'bg-black text-white text-2xl px-2 py-2 rounded-xl' : 'bg-black text-white text-2xl px-2 py-2 rounded-xl cursor-not-allowed'}
--- src/server-actions/serverActions.ts
+++ src/server-actions/serverActions.ts
@@ -3,36 +3,47 @@
 import {carService} from "@/services/car.api.service";
 import {revalidatePath} from "next/cache";
 import {redirect} from "next/navigation";
+import {CarSchema} from "@/validation/CarSсhema";
 // import {ICar} from "@/models/ICar";
 
-type StateType = {
+export type StateType = {
     payload?: FormData;
     errors?: Partial<{
-        customerId: string[];
-        amount: string[];
-        status: string[];
+        brand: string[];
+        year: string[];
+        price: string[];
     }>;
     message?: string | null;
 }
 
-export const createCar = async (prevState: StateType , formData: FormData) => {
-    console.log('> createCar', formData);
-    const brand = formData.get('brand') as string;
-    const year = Number(formData.get('year'));
-    const price = Number(formData.get('price'));
+export const createCar = async (prevState: StateType, formData: FormData): Promise<StateType> => {
+    console.log(formData);
+    // const brand = formData.get('brand') as string;
+    // const year = Number(formData.get('year'));
+    // const price = Number(formData.get('price'));
 
-    const car = {brand, year, price};
-    console.log(car);
+    const validatedFields = CarSchema.validate({
+        brand: formData.get('brand'),
+        year: formData.get('year'),
+        price: formData.get('price'),
+    });
 
-    try {
-        await carService.createCar(car);
-        // return {message: 'success', data};
-    } catch (e) {
-        console.log(e);
-        // return {message: 'fetch failed'};
-    } finally {
-        revalidatePath('/cars');
-        redirect('/cars');
+    if (!validatedFields.error) {
+        return {
+            payload: formData,
+            errors: validatedFields.error,
+            message: 'Invalid Fields',
+        };
     }
+    // const car = CarSchema.parse({brand, year, price});
+    // console.log(car);
+    // const { brand, year, price } = validatedFields.data;
+
+     console.log(validatedFields.value);
+    await carService.createCar(validatedFields.value);
+
+    revalidatePath('/cars')
+    redirect('/cars');
+
 };
 
