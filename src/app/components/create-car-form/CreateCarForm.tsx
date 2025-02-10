6409930c13dcae785b964fd3aef90b4eeb121839
fedorchaklm <fedorchak.luba@gmail.com>
2025-02-10 14:45:05 +0200

    hw3

--- src/app/components/create-car-form/CreateCarForm.tsx
+++ src/app/components/create-car-form/CreateCarForm.tsx
@@ -2,6 +2,7 @@
 
 import {createCar, State} from "@/server-actions/serverActions";
 import {useActionState} from "react";
+import Form from "next/form";
 
 const initialState = {
     message: '',
@@ -14,24 +15,27 @@ const CreateCarForm = () => {
     const price = state.payload?.get("price")?.toString() ?? 0;
     const year = state.payload?.get("year")?.toString() ?? 0;
 
+    console.log('>', state);
+
     return (
-        <form action={formAction} method="POST"
+        <Form action={formAction}
               className='flex flex-col items-center gap-4 text-2xl rounded-3xl w-fit px-8 py-4 m-auto my-4 bg-white'>
             <h1 className='text-3xl'>Create car</h1>
             <label htmlFor='brand'>Enter brand</label>
             <input className='border-2 border-gray-500 rounded-xl block pl-4 py-2' type='text' id='brand' name='brand'
                    placeholder='brand' defaultValue={brand}/>
-            {state.errors?.brand && <p className='text-red-500' aria-live="polite">{state.message}</p>}
+            {state.errors?.brand && <p className='text-red-500' aria-live="polite">{state.errors.brand}</p>}
             <label htmlFor='price'>Enter price</label>
             <input className='border-2 border-gray-500 rounded-xl block pl-4 py-2' type='number' id='price' name='price'
                    placeholder='price' defaultValue={price}/>
-            {state.errors?.price && <p className='text-red-500' aria-live="polite">{state.message}</p>}
+            {state.errors?.price && <p className='text-red-500' aria-live="polite">{state.errors.price}</p>}
             <label htmlFor='year'>Enter year</label>
             <input className='border-2 border-gray-500 rounded-xl block pl-4 py-2' type='number' id='year' name='year'
                    placeholder='year' defaultValue={year}/>
-            {state.errors?.year && <p className='text-red-500' aria-live="polite">{state.message}</p>}
+            {state.errors?.year && <p className='text-red-500' aria-live="polite">{state.errors.year}</p>}
             <button className='bg-black text-white text-2xl px-2 py-2 rounded-xl' type="submit">ADD</button>
-        </form>
+            {state.message && <p className='text-red-500' aria-live="polite">{state.message}</p>}
+        </Form>
     );
 };
 
--- src/server-actions/serverActions.ts
+++ src/server-actions/serverActions.ts
@@ -16,12 +16,18 @@ export type State = {
 
 const CarSchema = z.object({
     brand: z.string()
-        .min(1, {message: 'Min length 1 character'}),
-    year: z.number(),
-    price: z.number(),
+        .min(1, {message: 'Min length 1 character'})
+        .max(20, {message: 'Max length 20 characters'})
+        .regex(new RegExp('^[a-zA-Zа-яА-яёЁіІїЇєЄҐґ]{1,20}$'), {message: 'Max length 20 characters'}),
+    year: z.coerce.number()
+        .min(1990, {message: 'Min year 1990'})
+        .max(2024, {message: 'Max year 2024'}),
+    price: z.coerce.number()
+        .min(0, {message: 'Min price 0'})
+        .max(1000000, {message: 'Max price 1000000'}),
 });
 
-export const createCar = async (prevState: State, formData: FormData):  Promise<State> => {
+export const createCar = async (prevState: State, formData: FormData): Promise<State> => {
     console.log(formData);
     // const brand = formData.get('brand') as string;
     // const year = Number(formData.get('year'));
@@ -35,8 +41,9 @@ export const createCar = async (prevState: State, formData: FormData):  Promise<
 
     if (!validatedFields.success) {
         return {
+            payload: formData,
             errors: validatedFields.error.flatten().fieldErrors,
-            message: 'Missing Fields',
+            message: 'Invalid Fields',
         };
     }
     // const car = CarSchema.parse({brand, year, price});
