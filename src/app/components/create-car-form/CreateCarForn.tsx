940c0c90a4e225111d60e7f8521ed11fd4fed6a6
fedorchaklm <fedorchak.luba@gmail.com>
2025-02-10 16:56:17 +0200

    hw3

--- src/app/components/create-car-form/CreateCarForn.tsx
+++ src/app/components/create-car-form/CreateCarForn.tsx
@@ -2,62 +2,59 @@
 
 import {createCar} from "@/server-actions/serverActions";
 import {useForm} from "react-hook-form";
-import {z} from 'zod';
-import {zodResolver} from "@hookform/resolvers/zod";
 import {ICar} from "@/models/ICar";
-import {useRef} from "react";
+// import {useRef} from "react";
+import {joiResolver} from "@hookform/resolvers/joi";
+import {CarSchema} from "@/validation/CarSсhema";
 import Form from "next/form";
-
-const CarSchema = z.object({
-    brand: z.string().trim().min(1, {message: 'At least one char'}),
-    price: z.coerce.number().min(0, {message: 'Min 0'}).max(1000000, {message: 'Too much'}),
-    year: z.coerce.number().min(1990, {message: 'Min 1990'}).max(2024, {message: 'Max 2024'}),
-});
+import {useActionState} from "react";
 
 type CreateCar = Omit<ICar, "id">;
 
+const initialState = {
+    message: {}
+}
+
 const CreateCarForm = () => {
-    const {handleSubmit, register, formState: {errors}} = useForm<CreateCar>({
-        resolver: zodResolver(CarSchema),
-        defaultValues: {
-            brand: 'BMV',
-            price: 1000,
-            year: 2010
-        }
+    const {handleSubmit, register, formState: {errors, isValid}} = useForm<CreateCar>({
+        mode: "onChange", resolver: joiResolver(CarSchema)
     });
-    const formRef = useRef<HTMLFormElement>(null);
 
     console.log('> ', errors);
 
-    const onSubmit = async (data: CreateCar) => {
-        console.log('> onSubmit', data);
-        const formData = new FormData();
-        formData.append('brand', data.brand);
-        formData.append('price', `${data.price}`);
-        formData.append('year', `${data.year}`);
-        formRef.current?.submit();
-    };
+    // const onSubmit = async (data: CreateCar) => {
+    //     console.log('> onSubmit', data);
+    //     const formData = new FormData();
+    //     formData.append('brand', data.brand);
+    //     formData.append('price', `${data.price}`);
+    //     formData.append('year', `${data.year}`);
+    // };
+
+    const [state, formAction] = useActionState(createCar, initialState)
 
     return (
-        <Form ref={formRef} action={createCar} onSubmit={handleSubmit(onSubmit)}
+        <Form action={formAction} onSubmit={handleSubmit(onSubmit)}
               className='flex flex-col items-center gap-4 text-2xl rounded-3xl w-fit px-8 py-4 m-auto my-4 bg-white'>
             <h1 className='text-3xl'>Create car</h1>
             <label htmlFor='brand'>Enter brand</label>
             <input className='border-2 border-gray-500 rounded-xl block pl-4 py-2' type='text'
                    id='brand' {...register('brand')}
                    placeholder='brand'/>
-            <div>{errors.brand?.message}</div>
+            <div className='text-red-500'>{errors.brand?.message}</div>
             <label htmlFor='price'>Enter price</label>
             <input className='border-2 border-gray-500 rounded-xl block pl-4 py-2' type='number'
                    id='price' {...register('price')}
                    placeholder='price'/>
-            <div>{errors.price?.message}</div>
+            <div className='text-red-500'>{errors.price?.message}</div>
             <label htmlFor='year'>Enter year</label>
             <input className='border-2 border-gray-500 rounded-xl block pl-4 py-2' type='number'
                    id='year' {...register('year')}
                    placeholder='year'/>
-            <div>{errors.year?.message}</div>
-            <button className='bg-black text-white text-2xl px-2 py-2 rounded-xl' type="submit">ADD</button>
+            <div className='text-red-500'>{errors.year?.message}</div>
+            <button
+                className={isValid ? 'bg-black text-white text-2xl px-2 py-2 rounded-xl' : 'bg-black text-white text-2xl px-2 py-2 rounded-xl cursor-not-allowed'}
+                type="submit" disabled={!isValid}>ADD
+            </button>
         </Form>
     );
 };
--- src/server-actions/serverActions.ts
+++ src/server-actions/serverActions.ts
@@ -3,22 +3,36 @@
 import {carService} from "@/services/car.api.service";
 import {revalidatePath} from "next/cache";
 import {redirect} from "next/navigation";
+// import {ICar} from "@/models/ICar";
 
-export const createCar = async (formData: FormData) => {
-        console.log('> createCar', formData);
-        const brand = formData.get('brand') as string;
-        const year = Number(formData.get('year'));
-        const price = Number(formData.get('price'));
+type StateType = {
+    payload?: FormData;
+    errors?: Partial<{
+        customerId: string[];
+        amount: string[];
+        status: string[];
+    }>;
+    message?: string | null;
+}
 
-        const car = {brand, year, price};
-        console.log(car);
+export const createCar = async (prevState: StateType , formData: FormData) => {
+    console.log('> createCar', formData);
+    const brand = formData.get('brand') as string;
+    const year = Number(formData.get('year'));
+    const price = Number(formData.get('price'));
 
-        try {
-            await carService.createCar(car);
-        } catch (e) {
-            console.log(e);
-        }
-        revalidatePath('/cars')
+    const car = {brand, year, price};
+    console.log(car);
+
+    try {
+        await carService.createCar(car);
+        // return {message: 'success', data};
+    } catch (e) {
+        console.log(e);
+        // return {message: 'fetch failed'};
+    } finally {
+        revalidatePath('/cars');
         redirect('/cars');
     }
-;
+};
+
