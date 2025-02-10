0c8b5f6850743a0cb3f9c44379cdbc763635447b
fedorchaklm <fedorchak.luba@gmail.com>
2025-02-10 14:49:49 +0200

    hw3

--- src/app/components/create-car-form/CreateCarForn.tsx
+++ src/app/components/create-car-form/CreateCarForn.tsx
@@ -17,7 +17,7 @@ const CarSchema = z.object({
 type CreateCar = Omit<ICar, "id">;
 
 const CreateCarForm = () => {
-    const {handleSubmit, register, formState: { errors }} = useForm<CreateCar>({
+    const {handleSubmit, register, formState: {errors}} = useForm<CreateCar>({
         resolver: zodResolver(CarSchema),
         defaultValues: {
             brand: 'BMV',
@@ -39,23 +39,26 @@ const CreateCarForm = () => {
     };
 
     return (
-        <form ref={formRef} action={createCar} onSubmit={handleSubmit(onSubmit)}
+        <Form ref={formRef} action={createCar} onSubmit={handleSubmit(onSubmit)}
               className='flex flex-col items-center gap-4 text-2xl rounded-3xl w-fit px-8 py-4 m-auto my-4 bg-white'>
             <h1 className='text-3xl'>Create car</h1>
             <label htmlFor='brand'>Enter brand</label>
             <input className='border-2 border-gray-500 rounded-xl block pl-4 py-2' type='text'
                    id='brand' {...register('brand')}
                    placeholder='brand'/>
+            <div>{errors.brand?.message}</div>
             <label htmlFor='price'>Enter price</label>
             <input className='border-2 border-gray-500 rounded-xl block pl-4 py-2' type='number'
                    id='price' {...register('price')}
                    placeholder='price'/>
+            <div>{errors.price?.message}</div>
             <label htmlFor='year'>Enter year</label>
             <input className='border-2 border-gray-500 rounded-xl block pl-4 py-2' type='number'
                    id='year' {...register('year')}
                    placeholder='year'/>
+            <div>{errors.year?.message}</div>
             <button className='bg-black text-white text-2xl px-2 py-2 rounded-xl' type="submit">ADD</button>
-        </form>
+        </Form>
     );
 };
 
