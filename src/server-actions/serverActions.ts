ebc74096353a82454c28bdcbb8d08fc663b5ca4c
fedorchaklm <fedorchak.luba@gmail.com>
2025-02-11 23:30:13 +0200

    added cookies

--- src/server-actions/serverActions.ts
+++ src/server-actions/serverActions.ts
@@ -3,13 +3,21 @@
 import {carService} from "@/services/car.api.service";
 import {revalidatePath} from "next/cache";
 import {redirect} from "next/navigation";
+import {cookies} from 'next/headers';
+import {setCookie, getCookie} from 'cookies-next';
 
 export const createCar = async (formData: FormData) => {
+    const cookieStore = await cookies();
     const brand = formData.get('brand') as string;
     const year = Number(formData.get('year'));
     const price = Number(formData.get('price'));
     const car = {brand, year, price};
-
+    cookieStore.set('car', JSON.stringify(car));
+    await setCookie('key', 'value', {cookies});
+    const a = await getCookie('key', {cookies});
+    console.log(a);
+    const cookie = cookieStore.get('car');
+    console.log(cookie);
     try {
         await carService.createCar(car);
     } catch (e) {
